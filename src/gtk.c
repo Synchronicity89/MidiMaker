90b18f74da3a0e125ce9263c7456b5828079fd79
Clément <clement.landais.cml@gmail.com>
2019-07-07 21:01:48 +0200

    Create and initialize gtk.c/h files

---
+++ src/gtk.c
@@ -0,0 +1,7 @@
+/**\file gtk.c
+ * \brief This file aims to do the graphic interface of the program with gtk library
+ * \author Clement Landais
+ * \date 07.07.2019
+ */
+
+#include "gtk.h"
---
+++ src/gtk.h
@@ -0,0 +1,20 @@
+/**
+ * \file gtk.h
+ * \brief This file is the header file associated to gtk.c
+ * \author Clement Landais
+ * \date 07.07.2019
+ */
+
+#ifndef GTK_H
+#define GTK_H
+
+/* -includes in gui.h-
+#include <SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL_ttf.h>
+#include <limits.h>
+#include "global.h"
+#include "mainFunctions.h"
+#include <unistd.h>
+*/
